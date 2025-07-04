f9760f3e415f07fbd6e36714302cb9b268be46ba
MosesChisz <mosesc@cortexware.co.za>
2025-07-04 12:47:30 +0200

    callPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,7 +44,13 @@ const App = () => {
       ) : (
         <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
       )} />
-      <Route path="/call/:id" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+      <Route path="/call/:id" element={
+        isAuthenticated && isOnboarded ? (
+          <CallPage />
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )
+        } />
       <Route path="/chat/:id" element={
         isAuthenticated && isOnboarded ? (
           <Layout showSidebar={false}>
